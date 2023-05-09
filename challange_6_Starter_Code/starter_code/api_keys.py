77ac2fff8fcca1259f2d19a8f8e1e1ee51444ab1
nabroo101 <elnabawy101@gmail.com>
2023-05-09 12:17:09 -0400

    uploaded api keys and git ignore

--- challange_6_Starter_Code/starter_code/api_keys.py
+++ challange_6_Starter_Code/starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "7cb547d52cbe6a055ba464e6b414fe90"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "0d00cb9e9e1c4425b8c7f69bf3e6b655"
